e37127bc37b2db759494423a080023b9a81d805e
Dominic May <jack.thatch@gmail.com>
2014-10-26 10:53:55 +0800

    Try to handle more speadsheet formats

--- datastore/data_source/parser/parser.py
+++ datastore/data_source/parser/parser.py
@@ -1,5 +1,11 @@
-import xlrd
+# from pprint import pprint
+from dateutil.parser import parse as parse_date
 from itertools import chain
+import datetime
+import json
+import re
+
+import xlrd
 
 EXCLUDED_SHEET_NAMES = {
     'Contents'
@@ -57,64 +63,103 @@ def remove_invalid_chars(row):
     ]
 
 
-# def determine_type()
+def parse_release_date(row):
+    # slice the crap off the first cell
+    bit = row[0][12:].replace('(Canberra time)', 'GMT+11')
+    return parse_date(bit)
 
 
 def parse_sheet(sheet):
-    rows = list(sheet._cell_values)[3:-3]
-
     data = {}
-    data['meta'] = parse_sheet_header(rows.pop(0))
-    data['headers'] = [cell for cell in rows.pop(0) if cell]
 
-    with open('sheets/{}.csv'.format(sheet.name), 'w') as fh:
-        fh.write('\n'.join(
-            '<>'.join(map(str, row))
-            for row in rows
-        ))
+    rows = list(sheet._cell_values)[1:-3]
+
+    data.update({
+        'id': rows.pop(0)[0].split()[0],
+        'release_date': parse_release_date(rows.pop(0)),
+        'meta': parse_sheet_header(rows.pop(0))
+    })
+    rows.pop(0)
 
     with open('sheets/{}.html'.format(sheet.name), 'w') as fh:
         fh.write(to_html_table(rows))
 
+    # with open('sheets/{}.csv'.format(sheet.name), 'w') as fh:
+    #     fh.write('\n'.join(
+    #         '<>'.join(map(str, row))
+    #         for row in rows
+    #     ))
+
     sections = []
 
-    print(data['meta']['name'], data['headers'])
-    print(remove_invalid_chars(rows[0]))
-
     # cheat for the moment
-    rows = rows[2:]
+    row = remove_invalid_chars(rows[0])
+    if re.match(r'\d{4}-\d{2}', row[1]):
+        print(sheet.name, 'is by the months')
+        headers = remove_invalid_chars(rows.pop(0)[1:])
+
+    else:
+        print(sheet.name, 'is layered')
+
+        headers = []
+
+        parents = remove_invalid_chars(rows.pop(0)[1:])
+        children = remove_invalid_chars(rows.pop(0)[1:])
+        for parent, sub_header in zip(parents, children):
+            if parent:
+                current_parent_header = parent
+
+            if sub_header:
+                headers.append('{} - {}'.format(
+                    current_parent_header, sub_header
+                ))
+            else:
+                headers.append(parent)
+
+    data['scales'] = remove_invalid_chars(rows.pop(0)[1:])
 
     while rows:
         row = rows.pop(0)
 
         if len(clean(row)) == 1:
-            # gotta new sections
-            section_name = row[0].title()
-            if section_name.startswith('Rse'):
-                section_name = 'R.S.E.' + section_name[3:]
+            # gotta new section
+            if row[0].isupper():  # primary header
+                section_name = row[0].title()
+                if section_name.startswith('Rse'):
+                    section_name = 'R.S.E.' + section_name[3:]
 
-            sections.append((section_name, {}))
+                sections.append((section_name, {}))
+
+            else:
+                # subheader, ignore
+                pass
 
         else:
-            try:
+            # try:
                 sections[-1]
 
                 location, *values = row
 
+                assert len(headers) == len(values)
+
                 sections[-1][1][location] = dict(zip(
-                    data['headers'], values
+                    headers, values
                 ))
 
-            except IndexError:
-                return
+            # except IndexError as e:
+            #     print(e)
+            #     return
                 # print(sections)
                 # raise
 
-    from pprint import pprint
+    # from pprint import pprint
 
     # sections is a list of tuples, so yeah
-    sections = dict(sections)
-    pprint(sections)
+    data['sections'] = dict(sections)
+    filename = 'sheets/{}.json'.format(sheet.name)
+    with open(filename, 'w', encoding='utf-8') as fh:
+        json.dump(data, fh, indent=4, cls=SetSerializer)
+    # pprint(sections)
     # pprint([
     #     [
     #         str(cell).encode('utf-8')
@@ -124,6 +169,17 @@ def parse_sheet(sheet):
     # ])
 
 
+class SetSerializer(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+
+        elif isinstance(obj, datetime.datetime):
+            return obj.timestamp()
+
+        return super().default(obj)
+
+
 def read_file(filename):
     doc = xlrd.open_workbook(filename)
 
@@ -131,8 +187,9 @@ def read_file(filename):
         parse_sheet(sheet)
         for sheet in doc.sheets()
         # for sheet in [doc.sheets()[1]]
-        if sheet.name not in EXCLUDED_SHEET_NAMES and
-        sheet.name.startswith('Table')
+        if re.match(r'^Table \d+$', sheet.name)
+        # if sheet.name not in EXCLUDED_SHEET_NAMES and
+        # sheet.name.startswith('Table')
     ]
 
 
